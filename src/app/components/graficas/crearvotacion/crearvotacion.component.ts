59a00755e0040ccfb5c2d618eb905ccc144dc0b3
JhonyGms <jhonatan.garcia.ayala.99@gmail.com>
2021-09-16 00:37:06 -0500

    sector

--- src/app/components/graficas/crearvotacion/crearvotacion.component.ts
+++ src/app/components/graficas/crearvotacion/crearvotacion.component.ts
@@ -1,49 +1,45 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
-import { GraficasService } from '../../../services/graficas.service'
-import { MatDialog } from '@angular/material/dialog'
-import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component'
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
+import { GraficasService } from '../../../services/graficas.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-crearvotacion',
   templateUrl: './crearvotacion.component.html',
-  styleUrls: ['./crearvotacion.component.css']
+  styleUrls: ['./crearvotacion.component.css'],
 })
 export class CrearvotacionComponent implements OnInit {
+  @Input() id = {};
 
-    @Input() id = {}
-  
   user = {
     username: '',
-    password: ''
-  }
+    password: '',
+  };
 
-  get telefonos(){
+  get telefonos() {
     return this.registerForm.get('telefonos') as FormArray;
   }
 
-  constructor(
-    public formBuilder: FormBuilder,
-    public GraficasServices: GraficasService ,
-    public dialog: MatDialog
-    ) {
-      
-     }
+  constructor(public formBuilder: FormBuilder, public GraficasServices: GraficasService, public dialog: MatDialog) {}
 
-     registerForm = this.formBuilder.group({
-       id: [''],
-      nombre: ['', Validators.required],
-      diferentesOpciones: [false],
-      telefonos: this.formBuilder.array([])
+  registerForm = this.formBuilder.group({
+    id: [''],
+    nombre: ['', Validators.required],
+    diferentesOpciones: [false],
+    Check1: [true],
+    Check2: [true],
+    Check3: [true],
+    Check4: [true],
+    Check5: [true],
+    telefonos: this.formBuilder.array([]),
+  });
 
-    })
-    
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  agregarTelefono(){
-    const telefonoFormGroup  = this.formBuilder.group({
-      telefono: ''
+  agregarTelefono() {
+    const telefonoFormGroup = this.formBuilder.group({
+      telefono: '',
     });
     this.telefonos.push(telefonoFormGroup);
   }
@@ -52,42 +48,51 @@ export class CrearvotacionComponent implements OnInit {
     this.telefonos.removeAt(indice);
   }
 
-  enviar(value){
-    console.log(value)
+  enviar(value) {
+    console.log(value);
   }
 
   submit() {
-
     if (!this.registerForm.valid) {
       alert('Alguna regla de validación no se está cumpliendo');
       return;
     }
     this.registerForm.patchValue({
-      id: this.id['id']
+      id: this.id['id'],
     });
-    this.GraficasServices.qcrearvotacion(this.registerForm.value)
-    .subscribe(res => {
-      const dialogref = this.dialog.open(ConfirmDialogComponent,{data:{title:"Todo salio perfecto",text:"Se ha podido crear la votacion de forma satisfactoria"}})
-      dialogref.afterClosed()
-      .subscribe(res => {
-        
-      })
-    },
-    err => {
-      const dialogref = this.dialog.open(ConfirmDialogComponent,{data:{title:"Paso algo",text:"No se ha podido crear la votacion de forma satisfactoria"}})
-      dialogref.afterClosed()
-      .subscribe(res => {
-        
-      })
-    })
+    this.GraficasServices.qcrearvotacion(this.registerForm.value).subscribe(
+      (res) => {
+        const dialogref = this.dialog.open(ConfirmDialogComponent, {
+          data: {
+            title: 'Todo salio perfecto',
+            text: 'Se ha podido crear la votacion de forma satisfactoria',
+          },
+        });
+        dialogref.afterClosed().subscribe((res) => {});
+      },
+      (err) => {
+        const dialogref = this.dialog.open(ConfirmDialogComponent, {
+          data: {
+            title: 'Paso algo',
+            text: 'No se ha podido crear la votacion de forma satisfactoria',
+          },
+        });
+        dialogref.afterClosed().subscribe((res) => {});
+      }
+    );
     console.log(this.registerForm.value);
   }
 
   refrescar() {
     this.registerForm.patchValue({
       nombre: '',
-      diferentesOpciones: false
+      diferentesOpciones: false,
+      Check1: true,
+      Check2: true,
+      Check3: true,
+      Check4: true,
+      Check5: true,
     });
     this.telefonos.controls.splice(0, this.telefonos.length);
   }
-} 
+}
